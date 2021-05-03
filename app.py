ba32fc571deb011a7995eaa4cfdb16e3d0f45b3d
Matt Riese <rise.matt@gmail.com>
2021-05-03 12:34:54 -0700

    api/cupcakes route returns json

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, render_template, redirect, flash, jsonify
+
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Cupcake
+# from forms import AddPetForm
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "secret"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///cupcakes"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+connect_db(app)
+db.create_all()
+
+# Having the Debug Toolbar show redirects explicitly is often useful;
+# however, if you want to turn it off, you can uncomment this line:
+#
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+toolbar = DebugToolbarExtension(app)
+
+@app.route("/api/cupcakes")
+def show_all_cupcakes():
+    """get data about all cupcakes"""
+
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
--- models.py
+++ models.py
@@ -1,1 +1,45 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
+
+
+class Cupcake(db.Model):
+    """ Class for Cupcake """
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer,
+                    primary_key=True,
+                    autoincrement=True)
+    flavor = db.Column(db.String(30),
+                    nullable=False)
+    size = db.Column(db.Text,
+                        nullable=False)
+    rating = db.Column(db.Integer,
+                        nullable=False)
+    image = db.Column(db.Text,
+                    nullable=False,
+                    default="https://tinyurl.com/demo-cupcake")
+
+    def serialize(self):
+        """serialize to dictionary"""
+
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "size": self.size,
+            "rating": self.rating,
+            "image": self.image,
+        }
